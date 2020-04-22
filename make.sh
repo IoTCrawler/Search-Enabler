e71742c3d0998c894b1953828a1673bdbc845151
Pavel Smirnov <psmirnov@agtinternational.com>
2020-04-22 17:45:17 +0200

    Changes in deploy script/server

--- make.sh
+++ make.sh
@@ -34,7 +34,7 @@ if [ "$1" = "push-image" ]; then
 
 # gitlab.iotcrawler.net:4567/orchestrator/orchestrator is already in variables (on in a gitlab)
   echo "# docker tag ${CI_APPLICATION_REPOSITORY}:${CI_APPLICATION_TAG} ${CI_APPLICATION_TAG}"
-  docker tag ${CI_APPLICATION_REPOSITORY}:${CI_APPLICATION_TAG} ${CI_APPLICATION_TAG}
+  docker tag ${CI_APPLICATION_REPOSITORY}:${CI_APPLICATION_TAG} latest
   echo "# docker push ${CI_APPLICATION_REPOSITORY}:$CI_APPLICATION_TAG"
   docker push ${CI_APPLICATION_REPOSITORY}:$CI_APPLICATION_TAG
 	#docker push "${CI_APPLICATION_REPOSITORY}:latest"
