7d479042d278004f2641b36c7ba062e10b0e4f77
Pavel Smirnov <psmirnov@agtinternational.com>
2020-06-26 16:21:59 +0200

    POM modified

--- src/test/java/com/agtinternational/iotcrawler/graphqlEnabler/TestsHomeActivity.java
+++ src/test/java/com/agtinternational/iotcrawler/graphqlEnabler/TestsHomeActivity.java
@@ -1,17 +1,38 @@
 package com.agtinternational.iotcrawler.graphqlEnabler;
 
+/*-
+ * #%L
+ * search-enabler
+ * %%
+ * Copyright (C) 2019 - 2020 AGT International. Author Pavel Smirnov (psmirnov@agtinternational.com)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.agtinternational.iotcrawler.core.Utils;
 import com.agtinternational.iotcrawler.fiware.models.EntityLD;
 
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.ReadingBookActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WatchingTVActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WorkingOnComputerActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.ReadingBookActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WatchingTVActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WorkingOnComputerActivity;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -32,40 +53,40 @@ public class TestsHomeActivity extends TestUtils {
     }
 
     protected void initEntities(){
-        entities = createEntities();
-        //entities = readEntitiesFromFiles(new File("samples"));
+        //entities = createEntities();
+        entities = readEntitiesFromFiles(new File("samples","homeActivity"));
     }
 
-    protected List<EntityLD> createEntities(){
-        boolean cutURIs = false;
-
-        List<EntityLD> entities = new ArrayList<>();
-
-        ReadingBookActivity activity1 = new ReadingBookActivity("urn:reading:book1");
-        WatchingTVActivity activity2 = new WatchingTVActivity("urn:watching:tv1");
-        WorkingOnComputerActivity activity3 = new WorkingOnComputerActivity("urn:watching:tv1");
-
-        try {
-            entities.add(activity1.toEntityLD(cutURIs));
-            entities.add(activity2.toEntityLD(cutURIs));
-            entities.add(activity3.toEntityLD(cutURIs));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-        for(EntityLD entityLD: entities){
-            try {
-                Files.write(Paths.get("samples", Utils.getFragment(entityLD.getId().replace(":","-") + ".json")), Utils.prettyPrint(entityLD.toJsonObject()).getBytes());
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        return entities;
-    }
+//    protected List<EntityLD> createEntities(){
+//        boolean cutURIs = false;
+//
+//        List<EntityLD> entities = new ArrayList<>();
+//
+//        ReadingBookActivity activity1 = new ReadingBookActivity("urn:reading:book1");
+//        WatchingTVActivity activity2 = new WatchingTVActivity("urn:watching:tv1");
+//        WorkingOnComputerActivity activity3 = new WorkingOnComputerActivity("urn:watching:tv1");
+//
+//        try {
+//            entities.add(activity1.toEntityLD(cutURIs));
+//            entities.add(activity2.toEntityLD(cutURIs));
+//            entities.add(activity3.toEntityLD(cutURIs));
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//
+//        for(EntityLD entityLD: entities){
+//            try {
+//                Files.write(Paths.get("samples", Utils.getFragment(entityLD.getId().replace(":","-") + ".json")), Utils.prettyPrint(entityLD.toJsonObject()).getBytes());
+//            }
+//            catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return entities;
+//    }
 
 
     @Test
--- src/test/java/com/agtinternational/iotcrawler/graphqlEnabler/TestsSmartHome.java
+++ src/test/java/com/agtinternational/iotcrawler/graphqlEnabler/TestsSmartHome.java
@@ -1,135 +1,155 @@
-package com.agtinternational.iotcrawler.graphqlEnabler;
-
-import com.agtinternational.iotcrawler.core.Utils;
-import com.agtinternational.iotcrawler.fiware.models.EntityLD;
-import com.agtinternational.iotcrawler.graphqlEnabler.EnvVariablesSetter;
-import com.agtinternational.iotcrawler.graphqlEnabler.TestUtils;
-
-import com.agtinternational.iotcrawler.graphqlEnabler.TestsHomeActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.ReadingBookActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WatchingTVActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WorkingOnComputerActivity;
-import com.agtinternational.iotcrawler.smartHomeApp.models.*;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-public class TestsSmartHome extends TestUtils {
-    protected static Logger LOGGER = LoggerFactory.getLogger(TestsHomeActivity.class);
-
-    @Before
-    public void init(){
-        EnvVariablesSetter.init();
-    }
-
-    @Override
-    protected void initGraphQL() throws Exception {
-        List<String> paths = new ArrayList<>();
-        paths.add(Paths.get("schemas", "homeActivity.graphqls").toString());
-        paths.add(Paths.get("schemas", "smartHome.graphqls").toString());
-
-        initGraphQL(paths);
-    }
-
-    protected void initEntities(){
-        entities = createEntities();
-        //entities = readEntitiesFromFiles(new File("samples"));
-    }
-
-    protected List<EntityLD> createEntities(){
-        boolean cutURIs = false;
-
-        List<EntityLD> entities = new ArrayList<>();
-
-        Household houseHold1 = new Household("urn:household1");
-
-        HouseholdStateStream householdStateStream = new HouseholdStateStream("urn:household1:stateStream");
-        householdStateStream.observes(houseHold1);
-
-        HouseholdStateObservation householdStateObservation = new HouseholdStateObservation("urn:household1:stateObservation");
-        householdStateObservation.belongsTo(householdStateStream);
-
-        WatchingTVActivity watchingTVActivity = new WatchingTVActivity("urn:watching:tv1");
-        ReadingBookActivity readingBookActivity = new ReadingBookActivity("urn:reading:book1");
-        WorkingOnComputerActivity workingOnComputerActivity = new WorkingOnComputerActivity("urn:working:on:computer1");
-
-        householdStateObservation.addActivity(watchingTVActivity);
-        householdStateObservation.addActivity(readingBookActivity);
-        householdStateObservation.addActivity(workingOnComputerActivity);
-
-        householdStateObservation.removeActivity(workingOnComputerActivity);
-
-        try {
-            entities.add(houseHold1.toEntityLD(cutURIs));
-            entities.add(householdStateStream.toEntityLD(cutURIs));
-            entities.add(householdStateObservation.toEntityLD(cutURIs));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-        for(EntityLD entityLD: entities){
-            try {
-                Files.write(Paths.get("samples", Utils.getFragment(entityLD.getId().replace(":","-") + ".json")), Utils.prettyPrint(entityLD.toJsonObject()).getBytes());
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        return entities;
-    }
-
-
-    @Test
-    @Ignore
-    public void registerEntities() throws Exception {
-        initEntities();
-        super.registerEntities();
-    }
-
-    @Test
-    @Ignore
-    public void deleteEntities() throws Exception {
-        initEntities();
-        super.deleteEntities();
-    }
-
-    @Test
-    public void getHouseholds() throws Exception {
-
-        executeQuery(Paths.get("queries","smartHome","getHouseholds"));
-    }
-
-    @Test
-    public void getHouseholdStateStreams() throws Exception {
-
-        executeQuery(Paths.get("queries","smartHome","getHouseholdStateStreams"));
-    }
-
-    @Test
-    public void getHouseholdStateObservations() throws Exception {
-
-        executeQuery(Paths.get("queries","smartHome","getHouseholdStateObservations"));
-    }
-
-    @Test
-    public void getActivities() throws Exception {
-
-        executeQuery(Paths.get("queries","smartHome","getActivities"));
-    }
-
-    @Test
-    public void getAppliances() throws Exception {
-        executeQuery(Paths.get("queries","smartHome","getAppliances"));
-    }
-}
-
+//package com.agtinternational.iotcrawler.graphqlEnabler;
+//
+///*-
+// * #%L
+// * search-enabler
+// * %%
+// * Copyright (C) 2019 - 2020 AGT International. Author Pavel Smirnov (psmirnov@agtinternational.com)
+// * %%
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// * #L%
+// */
+//
+//import com.agtinternational.iotcrawler.core.Utils;
+//import com.agtinternational.iotcrawler.fiware.models.EntityLD;
+//import com.agtinternational.iotcrawler.graphqlEnabler.EnvVariablesSetter;
+//import com.agtinternational.iotcrawler.graphqlEnabler.TestUtils;
+//
+//import com.agtinternational.iotcrawler.graphqlEnabler.TestsHomeActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.ReadingBookActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WatchingTVActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.homeActivity.activity.WorkingOnComputerActivity;
+//import com.agtinternational.iotcrawler.smartHomeApp.models.*;
+//import org.junit.Before;
+//import org.junit.Ignore;
+//import org.junit.Test;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class TestsSmartHome extends TestUtils {
+//    protected static Logger LOGGER = LoggerFactory.getLogger(TestsHomeActivity.class);
+//
+//    @Before
+//    public void init(){
+//        EnvVariablesSetter.init();
+//    }
+//
+//    @Override
+//    protected void initGraphQL() throws Exception {
+//        List<String> paths = new ArrayList<>();
+//        paths.add(Paths.get("schemas", "homeActivity.graphqls").toString());
+//        paths.add(Paths.get("schemas", "smartHome.graphqls").toString());
+//
+//        initGraphQL(paths);
+//    }
+//
+//    protected void initEntities(){
+//        entities = createEntities();
+//        //entities = readEntitiesFromFiles(new File("samples"));
+//    }
+//
+//    protected List<EntityLD> createEntities(){
+//        boolean cutURIs = false;
+//
+//        List<EntityLD> entities = new ArrayList<>();
+//
+//        Household houseHold1 = new Household("urn:household1");
+//
+//        HouseholdStateStream householdStateStream = new HouseholdStateStream("urn:household1:stateStream");
+//        householdStateStream.observes(houseHold1);
+//
+//        HouseholdStateObservation householdStateObservation = new HouseholdStateObservation("urn:household1:stateObservation");
+//        householdStateObservation.belongsTo(householdStateStream);
+//
+//        WatchingTVActivity watchingTVActivity = new WatchingTVActivity("urn:watching:tv1");
+//        ReadingBookActivity readingBookActivity = new ReadingBookActivity("urn:reading:book1");
+//        WorkingOnComputerActivity workingOnComputerActivity = new WorkingOnComputerActivity("urn:working:on:computer1");
+//
+//        householdStateObservation.addActivity(watchingTVActivity);
+//        householdStateObservation.addActivity(readingBookActivity);
+//        householdStateObservation.addActivity(workingOnComputerActivity);
+//
+//        householdStateObservation.removeActivity(workingOnComputerActivity);
+//
+//        try {
+//            entities.add(houseHold1.toEntityLD(cutURIs));
+//            entities.add(householdStateStream.toEntityLD(cutURIs));
+//            entities.add(householdStateObservation.toEntityLD(cutURIs));
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//
+//        for(EntityLD entityLD: entities){
+//            try {
+//                Files.write(Paths.get("samples", Utils.getFragment(entityLD.getId().replace(":","-") + ".json")), Utils.prettyPrint(entityLD.toJsonObject()).getBytes());
+//            }
+//            catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return entities;
+//    }
+//
+//
+//    @Test
+//    @Ignore
+//    public void registerEntities() throws Exception {
+//        initEntities();
+//        super.registerEntities();
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void deleteEntities() throws Exception {
+//        initEntities();
+//        super.deleteEntities();
+//    }
+//
+//    @Test
+//    public void getHouseholds() throws Exception {
+//
+//        executeQuery(Paths.get("queries","smartHome","getHouseholds"));
+//    }
+//
+//    @Test
+//    public void getHouseholdStateStreams() throws Exception {
+//
+//        executeQuery(Paths.get("queries","smartHome","getHouseholdStateStreams"));
+//    }
+//
+//    @Test
+//    public void getHouseholdStateObservations() throws Exception {
+//
+//        executeQuery(Paths.get("queries","smartHome","getHouseholdStateObservations"));
+//    }
+//
+//    @Test
+//    public void getActivities() throws Exception {
+//
+//        executeQuery(Paths.get("queries","smartHome","getActivities"));
+//    }
+//
+//    @Test
+//    public void getAppliances() throws Exception {
+//        executeQuery(Paths.get("queries","smartHome","getAppliances"));
+//    }
+//}
+//
