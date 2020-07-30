03a3d193863e6209df7a592a2ffecf631d86ec20
Pavel Smirnov <psmirnov@agtinternational.com>
2020-07-30 11:48:11 +0200

    HasSimpleResult + Locations

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ if [ "$1" = "prepare-core" ]; then
 	echo "Search-enabler: Preparing core"
 	rm -rf /tmp/orchestrator && git clone https://github.com/IoTCrawler/Orchestrator.git /tmp/orchestrator
 	sed -i 's/<phase>process-sources<\/phase>/<phase>none<\/phase>/' /tmp/orchestrator/IoTCrawler/pom.xml
-	export CURR=$(pwd) && cd /tmp/orchestrator && git checkout -b 283c122513d41878721c89fd68c44e6145ebd33d && sh make.sh install && cd ${CURR}
+	export CURR=$(pwd) && cd /tmp/orchestrator && git checkout -b 8fe7722259a2944812e8c30ad5fee9d0cd564512 && sh make.sh install && cd ${CURR}
 fi
 
 if [ "$1" = "install" ]; then
